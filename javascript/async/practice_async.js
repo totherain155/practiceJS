60f617ac57681c0df98138b5d275a8daad1d3d68
totherain155 <totherain155@gmail.com>
2021-02-15 10:10:07 +0900

    practice_async

--- javascript/async/practice_async.js
+++ javascript/async/practice_async.js
@@ -1,3 +1,38 @@
+class UserStorage {
+    loginUser(id, password) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (
+                    (id === 'totherain' && password === 'dream') ||
+                    (id === 'coder' && password === 'academy')
+                ) {
+                    resolve(id)
+                } else {
+                    reject(new Error('not found'))
+                }
+            }, 2000);
+        })
+
+    }
+
+    getRoles(user) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (user === 'totherain') {
+                    resolve({ name: 'totherain', role: 'admin' })
+                } else {
+                    reject(new Error('no access'))
+                }
+            }, 1000)
+        })
+
+    }
+}
+
+
+const userStorage = new UserStorage()
+const id = prompt('enter your id');
+const password = prompt('enter your password')
 async function result() {
     try {
         const loginUser = await userStorage.loginUser(id, password);
@@ -7,6 +42,7 @@ async function result() {
         console.log(err);
     }
 }
+
 result();
 
 

2085424eb0835bd98ba0bbfb764c261881f7cce9
totherain155 <totherain155@gmail.com>
2021-03-13 11:45:59 +0900

    function-basic

--- javascript/async/practice_async.js
+++ javascript/async/practice_async.js
@@ -37,7 +37,7 @@ async function result() {
     try {
         const loginUser = await userStorage.loginUser(id, password);
         const getRoles = await userStorage.getRoles(loginUser);
-        return alert(`Hello ${loginUser}, you have a ${getRoles.role}`);
+        alert(`Hello ${loginUser}, you have a ${getRoles.role}`);
     } catch (err) {
         console.log(err);
     }
---
+++ javascript/function-basic.js
@@ -0,0 +1,26 @@
+//Don't give up
+
+
+//함수 선언
+
+
+function doSomething(add) {
+    console.log(add);
+    const result = add(2, 3);
+    console.log(result)
+}
+
+function add(a, b) {
+    const sum = a + b;
+    return sum;
+}
+
+
+
+//함수 호출 
+//doSomething(add);
+
+const addFun = add;
+console.log(addFun)
+const result1 = addFun(1, 2)
+console.log(result1)

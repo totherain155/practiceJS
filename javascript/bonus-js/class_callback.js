da118f2e2b4171d2287e6ce783b4c10e6140371e
totherain155 <totherain155@gmail.com>
2021-03-16 17:19:03 +0900

    practice

--- javascript/bonus-js/class_callback.js
+++ javascript/bonus-js/class_callback.js
@@ -1,7 +1,37 @@
 class Counter {
-    constructor() {
+    constructor(runEveryFiveTimes) {
         this.counter = 0;
+        this.callback = runEveryFiveTimes;
     }
 
+  increase() {
+      this.counter++;
+      console.log(this.counter);
+      if(this.counter % 5 == 0) {
+          this.callback && this.callback(this.counter)
     
+      }
+  }
+     
 }
+
+function printSomething(num) {
+    console.log(`yo! ${num}`)
+} 
+function alertNum(num) {
+    alert(`alert! ${num}`)
+}
+
+const printCounter = new Counter();
+const alertCounter = new Counter()
+
+printCounter.increase();  
+printCounter.increase();  
+printCounter.increase();  
+printCounter.increase();  
+printCounter.increase();  
+printCounter.increase();  
+printCounter.increase(); 
+printCounter.increase(); 
+printCounter.increase(); 
+alertCounter.increase(); 

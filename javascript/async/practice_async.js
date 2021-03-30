ecfd6fa2fd9001d9ecba012de13d89d4a1d17051
totherain155 <totherain155@gmail.com>
2021-03-30 20:43:46 +0900

    practice_async

--- javascript/async/practice_async.js
+++ javascript/async/practice_async.js
@@ -1,48 +1,44 @@
 class UserStorage {
-    loginUser(id, password) {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                if (
-                    (id === 'totherain' && password === 'dream') ||
-                    (id === 'coder' && password === 'academy')
-                ) {
-                    resolve(id)
-                } else {
-                    reject(new Error('not found'))
-                }
-            }, 2000);
-        })
+  loginUser(id, password) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (
+          (id === "totherain" && password === "dream") ||
+          (id === "coder" && password === "academy")
+        ) {
+          resolve(id);
+        } else {
+          reject(new Error("not found"));
+        }
+      }, 2000);
+    });
+  }
 
-    }
-
-    getRoles(user) {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                if (user === 'totherain') {
-                    resolve({ name: 'totherain', role: 'admin' })
-                } else {
-                    reject(new Error('no access'))
-                }
-            }, 1000)
-        })
-
-    }
+  getRoles(user) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (user === "totherain") {
+          resolve({ name: "totherain", role: "admin" });
+        } else {
+          reject(new Error("no access"));
+        }
+      }, 1000);
+    });
+  }
 }
 
-
-const userStorage = new UserStorage()
-const id = prompt('enter your id');
-const password = prompt('enter your password')
+const userStorage = new UserStorage();
+const id = prompt("enter your id");
+const password = prompt("enter your password");
 async function result() {
-    try {
-        const loginUser = await userStorage.loginUser(id, password);
-        const getRoles = await userStorage.getRoles(loginUser);
-        alert(`Hello ${loginUser}, you have a ${getRoles.role}`);
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const loginUser = await userStorage.loginUser(id, password);
+    const getRoles = await userStorage.getRoles(loginUser);
+    alert(`Hello ${loginUser}, you have a ${getRoles.role}`);
+    //alert(`Hello ${getRoles.name}, you have a ${getRoles.role}`);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 result();
-
-
